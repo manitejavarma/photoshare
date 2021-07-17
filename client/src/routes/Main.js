5f9d7db25fac877c9d2d5b61ac65785844f5bf9d
Anaik Chacon <chaconanaik@gmail.com>
2021-07-17 18:09:35 -0300

    Changed image uploader ui

--- client/src/routes/Main.js
+++ client/src/routes/Main.js
@@ -1,31 +1,66 @@
 import React from 'react';
 import ImageUploader from 'react-images-upload';
+import ReactDOM from "react-dom";
+import UploadComponent from "./Upload.jsx";
 
-class Home extends React.Component {
+import "./Main.css";
 
-    constructor(props) {
-        super(props);
-        this.state = { pictures: [] };
-        this.onDrop = this.onDrop.bind(this);
+class Main extends React.Component {
+  state = {
+    upload: {
+      pictures: [],
+      maxFileSize: 5242880,
+      imgExtension: [".jpg", ".png"],
+      defaultImages: [
+        "https://media.smarteragent.com/unsafe/http://cdn.photos.sparkplatform.com/fl/20190819183614687947000000-o.jpg",
+        "https://media.smarteragent.com/unsafe/http://cdn.photos.sparkplatform.com/fl/20190819183639357715000000-o.jpg",
+        "https://media.smarteragent.com/unsafe/http://cdn.photos.sparkplatform.com/fl/20190819183701098384000000-o.jpg"
+      ]
     }
+  };
 
-    onDrop(pictureFiles, pictureDataURLs) {
-        this.setState({
-            pictures: pictureFiles
-        });
-    }
+  handleChange = files => {
+    const { pictures } = this.state.upload;
+    console.warn({ pictures, files });
 
-    render() {
-        return (
-            <ImageUploader
-                withIcon={true}
-                buttonText='Choose images'
-                onChange={this.onDrop}
-                imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                maxFileSize={5242880}
-            />
-        );
-    }
+    this.setState(
+      {
+        ...this.state,
+        upload: {
+          ...this.state.upload,
+          pictures: [...pictures, ...files]
+        }
+      },
+      () => {
+        console.warn("It was added!");
+      }
+    );
+  };
+
+  confirmUpload = () => {
+    const { pictures, defaultImages } = this.state.upload;
+    console.warn("Confirm Upload =>", [...pictures]);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1 id="hello_user">Hello [username]</h1>
+
+        <hr />
+
+        <button id="confirm_button" onClick={this.confirmUpload}>Confirm upload</button>
+
+        <UploadComponent
+          {...this.state.upload}
+          handleChange={this.handleChange}
+        />
+      </div>
+    );
+  }
 }
 
-export default Home;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Main />, rootElement);
+
+export default Main;
