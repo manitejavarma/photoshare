05fe15170fe7bb7bbf90461dab2e04fe48120acd
manitejavarma <manitejavarma@dal.ca>
2021-07-25 20:44:45 -0300

    add modal for image

--- client/src/routes/Home.js
+++ client/src/routes/Home.js
@@ -8,6 +8,8 @@ import 'react-dropzone-uploader/dist/styles.css'
 import Dropzone from 'react-dropzone-uploader'
 import { Alert } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Lightbox from "react-image-lightbox";
+import "react-image-lightbox/style.css";
 
 const mapStateToProps = state => {
   return { session: state.session }
@@ -18,14 +20,16 @@ class Home extends Component {
     super(props)
     this.state = {
       uploaded: false,
-      images: []
+      images: [],
+      isOpen: false,
+      largeImageSrc: ''
     }
     this.handleUpload = this.handleUpload.bind(this)
     this.setUploaded = this.setUploaded.bind(this)
   }
-  
-  componentDidMount()  {
-    if(this.props.session.isLoggedIn) {
+
+  componentDidMount() {
+    if (this.props.session.isLoggedIn) {
       this.getImages(this.props.session.user.sub)
     }
   }
@@ -48,66 +52,76 @@ class Home extends Component {
 
   // Handles file upload event and updates state
   handleUpload = (files, allFiles) => {
-    if(files[0].meta.status === "done") {
+    if (files[0].meta.status === "done") {
       UserService.uploadImage(this.props.session.user.sub, files[0].file)
-      .then(result => {
-		this.setState(prevState => ({
-			
-		  images: [...prevState.images, result.data[1].data]
-		}))
-        //this.getImages(this.props.session.user.sub, true)
-        files[0].remove()
-      })
-      
+        .then(result => {
+          this.setState(prevState => ({
+
+            images: [...prevState.images, result.data[1].data]
+          }))
+          //this.getImages(this.props.session.user.sub, true)
+          files[0].remove()
+        })
+
     }
+  }
+
+  setLargeImage = (e) => {
+    console.log(e.target.currentSrc)
+    this.setState({ isOpen: true, largeImageSrc: e.target.currentSrc })
   }
 
   getImages(sub, upload = false) {
     axios.get('/getImages', {
       params: {
-         sub: sub
+        sub: sub
       }
     }).then((response) => {
-       let jsonReponse = response.data
-       const img = []
-       for (let i = 1; i < jsonReponse.length; i++) {
-         img[i] = jsonReponse[i].data
-       }
-      this.setState({uploaded: upload, images: img})
+      let jsonReponse = response.data
+      const img = []
+      for (let i = 1; i < jsonReponse.length; i++) {
+        img[i] = jsonReponse[i].data
+      }
+      this.setState({ uploaded: upload, images: img })
     })
   }
-    
-  render () {
+
+  render() {
     return (
       <div className="Home">
-          { this.props.session.isLoggedIn ? (
+        {this.props.session.isLoggedIn ? (
           <div className="App">
             <h1 id="hello_user">Hello {this.props.session.user.userName}, email: {this.props.session.user.email}</h1>
             <hr />
             <Dropzone
-              onChangeStatus = {this.handleChangeStatus}
-              onSubmit = {this.handleUpload}
-              maxFiles = {1}
-              inputContent = "Upload or Drag. File"
+              onChangeStatus={this.handleChangeStatus}
+              onSubmit={this.handleUpload}
+              maxFiles={1}
+              inputContent="Upload or Drag. File"
               accept="image/*"
-            /> 
-	          <br/>
-            {this.state.uploaded ? <Alert variant="success" onClose= {() => this.setUploaded(false)} dismissible>The image was uploaded successfully!</Alert>: null}
-            {this.state.images.map(image =>(
-                <img src={image}/>
+            />
+            <br />
+            {this.state.uploaded ? <Alert variant="success" onClose={() => this.setUploaded(false)} dismissible>The image was uploaded successfully!</Alert> : null}
+            {this.state.images.map(image => (
+              <img src={image} onClick={this.setLargeImage} />
             ))}
-	          <p> </p>
+            {this.state.isOpen && (
+              <Lightbox
+                mainSrc={this.state.largeImageSrc}
+                onCloseRequest={() => this.setState({ isOpen: false })}
+              />
+            )}
             <a className="Home-link" href="#" onClick={this.onSignOut}>Sign out</a>
           </div>
-          ) : (
-            <div id="big_container">
-              <div id="container">
-                <h1 class="header">Group 25</h1>
-                <p class="not_logged_in">You are not logged in!</p>
-                <a className="Home-link" href={cognitoUtils.getCognitoSignInUri()}>Sign in</a>
-              </div>
+        ) : (
+          <div id="big_container">
+            <div id="container">
+              <h1 class="header">Group 25</h1>
+              <p class="not_logged_in">You are not logged in!</p>
+              <a className="Home-link" href={cognitoUtils.getCognitoSignInUri()}>Sign in</a>
             </div>
-          )}
+          </div>
+        )}
       </div>
     )
   }
